75e51a75824face4b0e5bbd0718bcfd6f57ae039
sadmanador <84454116+sadmanador@users.noreply.github.com>
2023-07-26 18:42:40 +0000

    2 problems are solved

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ function strReverse(str) {
     reversedStr.push(str[i]);
   }
   const result = reversedStr.join("");
-  console.log(result)
+  console.log(result);
 }
 strReverse("hello world");
 
 //problem - 2
-function 
+function positiveSum(arr) {
+  const positiveArr = arr.filter((item) => item >= 0);
+  const sum = positiveArr.reduce((previous, current)=> previous + current, 0)
+  console.log(sum);
+}
+positiveSum([2, -5, 10, -3, 7] )
