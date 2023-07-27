5e09e3c017f54f51843629487cd1696f20737309
sadmanador <sadman.ador@gmail.com>
2023-07-27 20:18:04 +0600

    6th problem solved

--- index.js
+++ index.js
@@ -77,3 +77,26 @@ calculate(100, "/", 3);
 
 
 
+//problem - 6
+function generateRandomPassword(length) {
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const numberChars = "0123456789";
+  const specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+  const allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allChars.length);
+    password += allChars.charAt(randomIndex);
+  }
+
+  return console.log(password);
+}
+
+generateRandomPassword(16);
+
+
+
+
